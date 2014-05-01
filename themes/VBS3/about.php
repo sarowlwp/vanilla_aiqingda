61be45929856bd08bca416ed2ea45c7205ec6672
sarowliu <sarowliu@sohu-inc.com>
2014-05-01 22:41:29 +0800

    add css

--- themes/VBS3/about.php
+++ themes/VBS3/about.php
@@ -8,7 +8,7 @@
 $ThemeInfo['VBS3'] = array(
     "Name" => "VBS3",
     "Description" => "This theme was ported from Vanilla Forums Inc's hosted theme \"Bootstrap\" it has been modified to merge with Bootstrap 3 ( from 2.3 ) and has been designed for use on the OS model.",
-    "Version" => "1.1.5",
+    "Version" => "1.1.5.1",
     "RequiredApplications" => array("Vanilla" => "2.1b2"), //Do not edit me, this has been tested as a requirement
     "Author" => "Vanilla Forums Inc & Chris Ireland",
 );

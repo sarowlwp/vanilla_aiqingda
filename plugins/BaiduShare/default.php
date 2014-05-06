78fc4180863c0206933c34acb611d88a5990417d
sarowliu <sarowliu@sohu-inc.com>
2014-05-06 22:28:23 +0800

    fix baidu

--- plugins/BaiduShare/default.php
+++ plugins/BaiduShare/default.php
@@ -12,7 +12,7 @@ $PluginInfo['BaiduShare'] = array('Name' => 'BaiduShare', 'Description' => 'Inse
 //
 class BaiduSharePlugin extends Gdn_Plugin
 {
-  
+
   public static $ButtonCode = '<!-- Baidu Button BEGIN -->
   <div id="bdshare" class="bdshare_t bds_tools get-codes-bdshare">
     <span class="bds_more">分享到：</span>
@@ -26,33 +26,28 @@ class BaiduSharePlugin extends Gdn_Plugin
     <a class="shareCount"></a>
   </div>
   <!-- Baidu Button END -->';
-  
+
   public static $JSCode = '
   <script type="text/javascript" id="bdshare_js" data="type=slide&mini=1&amp;uid=672069&amp;img=2&amp;pos=left" ></script> 
   <script type="text/javascript" id="bdshell_js"></script>
   <script type="text/javascript">
     document.getElementById("bdshell_js").src = "http://bdimg.share.baidu.com/static/js/shell_v2.js?cdnversion=" + new Date().getHours();
   </script>';
-  
+
     //
     // We want to get our hot selves into the page before it renders and
     // what-not so that we can get our groove on and start tracking our
     // forum users... in a cool way; not in a creepy stalker way.
     //
-  public function DiscussionController_BeforeDiscussionRender_Handler(&$Sender) {
-  }
-  
+
   public function DiscussionController_BeforeCommentBody_Handler(&$Sender) {
     if ($Sender->EventArguments['Type'] == 'Discussion') {
       echo '喜欢就分享吧，让更多的人看到，感谢支持 ~ <br>' . self::$ButtonCode . '<br><br>';
-    }
-  }
-  
-  public function DiscussionController_AfterCommentBody_Handler(&$Sender) {
-    if ($Sender->EventArguments['Type'] == 'Discussion') {
-      echo self::$ButtonCode . '<br><br>';
       echo self::$JSCode;
     }
   }
-  
+
+  public function DiscussionController_AfterCommentBody_Handler(&$Sender) {
+  }
+
 }
